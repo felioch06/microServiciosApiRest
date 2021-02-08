b1a41569c7aeed4d2205562f3907b9d4339a2b25
Felipe Chacon <felipipe88806@gmail.com>
2021-02-07 21:38:43 -0500

    [ADD] Init

---
+++ src/main/java/com/app/productos/ProyectoMicroserviciosApplication.java
@@ -0,0 +1,13 @@
+package com.app.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMicroserviciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMicroserviciosApplication.class, args);
+	}
+
+}
